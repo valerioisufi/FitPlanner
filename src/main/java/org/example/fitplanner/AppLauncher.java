c33456eee5d5f4eb0bb4dd6d1d4555d124d0e9fc
valerioisufi <66517412+valerioisufi@users.noreply.github.com>
2026-01-08 02:37:08 +0100

    Definizione delle prime view

--- src/main/java/org/example/fitplanner/AppLauncher.java
+++ src/main/java/org/example/fitplanner/AppLauncher.java
@@ -1,8 +1,11 @@
 package org.example.fitplanner;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.example.fitplanner.view.HomeView;
+import org.example.fitplanner.view.RegistrationView;
 
 import java.util.Objects;
 
@@ -16,6 +19,13 @@ public class AppLauncher extends Application {
 
         stage.setTitle("FitPlanner");
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/appIcon.png"))));
+
+        var scene = new Scene(new RegistrationView());
+
+        String css = Objects.requireNonNull(getClass().getResource("/style/style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.setScene(scene);
         stage.show();
 
     }
---
+++ src/main/java/org/example/fitplanner/view/HomeView.java
@@ -0,0 +1,25 @@
+package org.example.fitplanner.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class HomeView extends VBox {
+    private Button primaryButton;
+
+    public HomeView() {
+        this.setSpacing(16);
+        this.setAlignment(Pos.CENTER);
+        this.setStyle("-fx-background-color: linear-gradient(to bottom right, #F8FAFC, #F1F5F9);");
+
+        primaryButton = new Button("Get Started");
+        primaryButton.setPrefSize(200, 50);
+        primaryButton.getStyleClass().add("primary-button");
+
+        this.getChildren().add(new Label("Welcome to FitPlanner!"));
+        this.getChildren().add(primaryButton);
+
+    }
+
+}
