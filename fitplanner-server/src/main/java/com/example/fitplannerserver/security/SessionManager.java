d2312e7cbb8165bdb34b42db6c33d0e5cf2b0dfa
valerioisufi <66517412+valerioisufi@users.noreply.github.com>
2026-01-13 22:41:19 +0100

    Recupero dell'utente che ha effettuato la richiesta tramite SessionManager

---
+++ fitplanner-server/src/main/java/com/example/fitplannerserver/security/SessionManager.java
@@ -0,0 +1,15 @@
+package com.example.fitplannerserver.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SessionManager {
+
+    public static String getUsername(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("Utente non autenticato");
+        }
+        return authentication.getName();
+    }
+}
