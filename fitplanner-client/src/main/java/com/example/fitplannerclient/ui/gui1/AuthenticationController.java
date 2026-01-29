824694ca74578076692328e503c5e800843b67b3
valerioisufi <valerioisufi@gmail.com>
2026-01-29 19:08:22 +0100

    feat: aggiorna AuthenticationController e AuthenticationView per supportare la nuova logica di accesso

--- fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/AuthenticationController.java
+++ fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.example.fitplannerclient.ui.gui1;
 
 import com.example.fitplannerclient.service.AuthenticationBoundary;
 import com.example.fitplannerclient.ui.GraphicController;
+import com.example.fitplannerclient.ui.gui1.view.AuthenticationView;
 import com.example.fitplannerclient.ui.gui1.view.RegistrationView;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,21 +10,27 @@ import javafx.stage.Stage;
 import java.util.Objects;
 
 public class AuthenticationController implements GraphicController {
-    RegistrationView view;
+    AuthenticationBoundary authenticationBoundary;
+    AuthenticationView view;
 
     public AuthenticationController(AuthenticationBoundary authenticationBoundary) {
-        view = new RegistrationView();
+        this.authenticationBoundary = authenticationBoundary;
 
-        String themeCss = Objects.requireNonNull(getClass().getResource("/style/style.css")).toExternalForm();
+        view = new AuthenticationView();
+        String themeCss = Objects.requireNonNull(getClass().getResource("/style/theme1.css")).toExternalForm();
         String iconsCss = Objects.requireNonNull(getClass().getResource("/style/icons.css")).toExternalForm();
         view.getStylesheets().addAll(themeCss, iconsCss);
     }
 
+
+    private void login(){
+
+    }
+
+
     @Override
     public void start(Stage stage) {
         stage.setScene(new Scene(view));
-        Scene scene = stage.getScene();
-        scene.setRoot(view);
         stage.show();
     }
 }
--- fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/view/AuthenticationView.java
+++ fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/view/AuthenticationView.java
@@ -1,23 +1,90 @@
 package com.example.fitplannerclient.ui.gui1.view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 public class AuthenticationView extends BaseView {
+
+    private final TextField usernameFieldInput = new TextField();
+    private final TextField passwordFieldInput = new TextField();
+
+    private final Button btnBack = new Button("Back");
+    private final Button btnNext = new Button("Next");
+
     public AuthenticationView() {
+        VBox registrationCard = createLoginForm();
+        StackPane.setAlignment(registrationCard, Pos.CENTER);
+        this.getChildren().add(registrationCard);
     }
 
-    private VBox createField(String labelText, String placeholder) {
+    public void setBtnBackAction(Runnable action) {
+        btnBack.setOnAction(event -> action.run());
+    }
+
+    public void setBtnNextAction(Runnable action) {
+        btnNext.setOnAction(event -> action.run());
+    }
+
+    public Button getBtnNext() {
+        return btnNext;
+    }
+
+    public String getUsername() {
+        return usernameFieldInput.getText();
+    }
+
+    public String getPassword() {
+        return passwordFieldInput.getText();
+    }
+
+    private VBox createLoginForm() {
+        VBox formContainer = new VBox();
+
+        VBox usernameField = createField("Username", "Inserisci lo username", this.usernameFieldInput);
+        VBox passwordField = createField("Password", "Inserisci la tua password", this.passwordFieldInput);
+
+        formContainer.getChildren().addAll(usernameField, passwordField);
+
+        Separator separator = new Separator();
+        separator.setPadding(new Insets(20, 0, 20, 0));
+
+        HBox buttonBox = new HBox();
+        buttonBox.setAlignment(Pos.CENTER_RIGHT);
+
+        btnBack.getStyleClass().add("button-secondary");
+        btnNext.getStyleClass().add("button-primary");
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        buttonBox.getChildren().addAll(btnBack, spacer, btnNext);
+
+        VBox card = new VBox();
+        card.getStyleClass().add("card");
+
+        card.setMinWidth(300);
+        card.setMaxWidth(400);
+        card.setMaxHeight(Region.USE_PREF_SIZE);
+
+        card.getChildren().addAll(formContainer, separator, buttonBox);
+
+        return card;
+    }
+
+    private VBox createField(String labelText, String placeholder, TextField targetField) {
         Label label = new Label(labelText);
         label.getStyleClass().add("label-field");
 
-        TextField textField = new TextField();
-        textField.setPromptText(placeholder);
-        textField.getStyleClass().add("text-field"); // Applica lo stile CSS
+        targetField.setPromptText(placeholder);
+        targetField.getStyleClass().add("text-field");
 
-        VBox fieldGroup = new VBox(5); // 5px spazio tra label e input
-        fieldGroup.getChildren().addAll(label, textField);
+        VBox fieldGroup = new VBox(5);
+        fieldGroup.getChildren().addAll(label, targetField);
         return fieldGroup;
     }
 }
