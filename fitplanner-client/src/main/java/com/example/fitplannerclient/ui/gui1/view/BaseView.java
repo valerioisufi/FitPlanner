cc23ea342282143264e6e9335d8152218216e9b2
Valerio Isufi <valerioisufi@gmail.com>
2026-01-29 17:33:09 +0100

    feat: aggiungi metodo per mostrare notifiche con animazione nel BaseView

--- fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/view/BaseView.java
+++ fitplanner-client/src/main/java/com/example/fitplannerclient/ui/gui1/view/BaseView.java
@@ -1,8 +1,48 @@
 package com.example.fitplannerclient.ui.gui1.view;
 
-public class BaseView {
+import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+public class BaseView extends StackPane {
     public BaseView() {
     }
 
+
+
+    protected void showNotification(String message) {
+        Label notification = new Label(message);
+        notification.getStyleClass().add("notification-toast"); // Stile CSS
+
+        notification.setWrapText(true);
+        notification.setMaxWidth(300);
+
+        StackPane.setAlignment(notification, Pos.BOTTOM_RIGHT);
+        StackPane.setMargin(notification, new Insets(0, 0, 20, 0));
+
+        Platform.runLater(() -> {
+            this.getChildren().add(notification);
+
+            fadeAnimation(notification);
+        });
+    }
+
+    private void fadeAnimation(Label notification) {
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
+
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), notification);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+
+        fadeOut.setOnFinished(e -> this.getChildren().remove(notification));
+
+        delay.setOnFinished(e -> fadeOut.play());
+        delay.play();
+    }
 
 }
