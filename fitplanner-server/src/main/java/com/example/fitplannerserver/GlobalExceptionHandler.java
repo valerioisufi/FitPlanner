be88b546a833b7dfb40e74547ae80a63bd9bbd09
Valerio Isufi <valerioisufi@gmail.com>
2026-01-30 17:56:38 +0100

    feat: aggiungi gestione delle eccezioni per risorse non trovate e accesso non autorizzato

--- fitplanner-server/src/main/java/com/example/fitplannerserver/GlobalExceptionHandler.java
+++ fitplanner-server/src/main/java/com/example/fitplannerserver/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.fitplannerserver;
 
 import com.example.fitplannerserver.exception.InvalidCredentialsException;
+import com.example.fitplannerserver.exception.ResourceNotFoundException;
+import com.example.fitplannerserver.exception.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +11,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
     @ExceptionHandler(InvalidCredentialsException.class)
     public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage()); // 401
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    // 404 - Risorsa non trovata (es. scheda, utente)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
 }
