bcd29457e74477b01c94a1e356d944f8d739dba0
Valerio Isufi <valerioisufi@gmail.com>
2026-07-05 02:23:37 +0200

    refactor: update password hashes for trainer and athlete accounts in SQL data

--- database/init/02-data.sql
+++ database/init/02-data.sql
@@ -3,18 +3,18 @@
 
 -- 1. Inserimento Trainer
 INSERT INTO accounts (user_id, email, password_hash, refreshToken, profile_type) VALUES 
-('11111111-1111-1111-1111-111111111111', 'trainer@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'TRAINER');
+('11111111-1111-1111-1111-111111111111', 'trainer@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'TRAINER');
 
 INSERT INTO profiles (user_id, first_name, last_name, contact_email, phone_number, profile_type, invitation_code, trainer_id) VALUES
 ('11111111-1111-1111-1111-111111111111', 'Super', 'Trainer', 'trainer@fitplanner.com', '1234567890', 'TRAINER', 'TRAINER-1234', NULL);
 
 -- 2. Inserimento Atleti
 INSERT INTO accounts (user_id, email, password_hash, refreshToken, profile_type) VALUES 
-('22222222-2222-2222-2222-222222222222', 'athlete@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'ATHLETE'),
-('33333333-3333-3333-3333-333333333333', 'mario@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'ATHLETE'),
-('44444444-4444-4444-4444-444444444444', 'luigi@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'ATHLETE'),
-('55555555-5555-5555-5555-555555555555', 'giulia@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'ATHLETE'),
-('66666666-6666-6666-6666-666666666666', 'anna@fitplanner.com', '$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HCGFGLn0O51OkmrJ.59pW', NULL, 'ATHLETE');
+('22222222-2222-2222-2222-222222222222', 'athlete@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'ATHLETE'),
+('33333333-3333-3333-3333-333333333333', 'mario@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'ATHLETE'),
+('44444444-4444-4444-4444-444444444444', 'luigi@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'ATHLETE'),
+('55555555-5555-5555-5555-555555555555', 'giulia@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'ATHLETE'),
+('66666666-6666-6666-6666-666666666666', 'anna@fitplanner.com', '$2a$10$dvz0Ia9SgTROIAWxgc1s8.B7GE8jg/sjVMzLzRo9Eo63gzbK8Cg16', NULL, 'ATHLETE');
 
 INSERT INTO profiles (user_id, first_name, last_name, contact_email, phone_number, profile_type, invitation_code, trainer_id) VALUES
 ('22222222-2222-2222-2222-222222222222', 'John', 'Doe', 'athlete@fitplanner.com', '0000000000', 'ATHLETE', NULL, '11111111-1111-1111-1111-111111111111'),
